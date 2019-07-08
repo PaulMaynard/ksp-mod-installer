73ad8a33f1281d1574188acbd846047d42b74927
Paul <palu.maynard@gmail.com>
2019-07-07 23:13:47 -0400

    Actually copies now

---
+++ README.md
@@ -0,0 +1,12 @@
+# KSP Mod Installer
+
+Are you tired of installing mods manually by downloading endless zips from 
+spacedock, hunding for their GameData folders, and dragging them into your
+KSP install? Do you also want to avoind the minefield that is CKAN, or you want
+to use a custom or dev version of a mod? Then this tool is for you!
+
+## Usage
+
+```bash
+python main.py [PATH]
+```
--- main.py
+++ main.py
@@ -25,26 +25,33 @@ def find_gamedata(mod):
     """
     Determines GameData location in mod directory
     """
-    datas = list(mod.glob("**/GameData/"))
+    datas = list(mod.rglob("GameData/"))
     if len(datas) == 0:
         if input(f"Couldn't find GameData, use directory root? [y]/n: ") in ("y", "Y", ""):
-            return mod
+            return [mod]
     elif len(datas) == 1:
         if input(f"Found GameData at {datas[0]}, use? [y]/n: ") in ("y", "Y", ""):
-            return datas[0]
+            return [datas[0]]
     else:
-        print("Found multiple GameDatas, choose one:")
+        print("Found multiple GameDatas, choose one, or 'a' to use all:")
         for i, gd in enumerate(datas):
             print(f"[{i}]: {gd}")
         i = input()
-        if i:
-            return datas[int(i)]
+        if i in ("a", "A"):
+            return datas
+        elif i:
+            return [datas[int(i)]]
     
-    return mod / input("Enter GameData location in mod directory:\n")
+    return [mod / input("Enter GameData location in mod directory:\n")]
+
+def install(src, dest):
+    for f in src.iterdir():
+        print(f"{f} => {dest / f.name}")
+        f.rename(dest / f.name)
 
 def main():
-    loc = get_folder()
-    print(f"installing to {loc}...\n")
+    ksp = get_folder()
+    print(f"installing to {ksp}...\n")
     if len(sys.argv) > 1:
         mod_folder = Path(sys.argv[1])
     else:
@@ -53,8 +60,11 @@ def main():
     if not mod_folder.is_dir():
         print(f"Directory {mod_folder} not found!")
         quit()
-    data = find_gamedata(mod_folder)
-    print(f"Installing from {data}...")
+    datas = find_gamedata(mod_folder)
+    for d in datas:
+        print(f"Installing from {d} ...")
+        install(d, ksp)
+    print("Done!")
 
 if __name__ == "__main__":
     main()
